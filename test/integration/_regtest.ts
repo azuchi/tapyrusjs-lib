dc4a8a158697aeab7f550cc10adafabbad8ec469
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-10-16 01:31:30 +0900

    Change dependencies and library name

--- test/integration/_regtest.ts
+++ test/integration/_regtest.ts
@@ -1,4 +1,4 @@
-import { RegtestUtils } from 'regtest-client';
+import { RegtestUtils } from 'tapyrusjs-client';
 
 const APIPASS = process.env.APIPASS || 'satoshi';
 const APIURL = process.env.APIURL || 'https://regtest.bitbank.cc/1';
